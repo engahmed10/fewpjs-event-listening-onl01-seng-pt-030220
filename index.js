65eb62a4e5cfebf1f34d14d73ddddf3a85273f17
AHMED AFANDY <eng.computer1@yahoo.com>
2020-08-27 03:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function addingEventListener() {
 
 const =document.getElementById("input")
 input.addEventListener('click',function(event){
-  aler("was cilicked !");
+  aler('was cilicked !');
 });
